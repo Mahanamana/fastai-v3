a8ec1619f41228b9103c84fe20321c7d83a01ffc
Andriamiharisoa <50105412+Mahanamana@users.noreply.github.com>
2020-05-11 17:21:19 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -31,16 +31,10 @@ async def download_file(url, dest):
 
 async def setup_learner():
     await download_file(export_file_url, path/export_file_name)
-    try:
-        learn = load_learner(path, export_file_name)
-        return learn
-    except RuntimeError as e:
-        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
-            print(e)
-            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
-            raise RuntimeError(message)
-        else:
-            raise
+    print('end download')
+    learn = load_learner(path, export_file_name)
+        
+    return learn
 
 
 loop = asyncio.get_event_loop()
